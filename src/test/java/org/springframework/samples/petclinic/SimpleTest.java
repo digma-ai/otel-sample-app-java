5ee4813fe14a834d6eb89eb76cba9c691a45426c
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-06-16 23:30:27 +0300

    simple test

---
+++ src/test/java/org/springframework/samples/petclinic/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+
+	@Test
+	void simple() {
+		System.out.println("in simple test");
+	}
+
+}
