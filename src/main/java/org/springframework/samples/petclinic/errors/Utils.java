e8c683527b64b80fb34e50cd016a03561a7a5e4a
Shay Keren <shay.keren@gmail.com>
2025-06-09 15:37:14 +0300

    docs: add documentation for throwException method usage and testing

--- src/main/java/org/springframework/samples/petclinic/errors/Utils.java
+++ src/main/java/org/springframework/samples/petclinic/errors/Utils.java
@@ -1,8 +1,6 @@
 package org.springframework.samples.petclinic.errors;
 
-import org.apache.coyote.BadRequestException;
-
-public class Utils {
+import org.apache.coyote.BadRequestException;public class Utils {
 	public static void ThrowIllegalStateException() throws IllegalStateException {
 		throw new IllegalStateException("Some unexpected state error");
 	}
@@ -13,16 +11,24 @@ public class Utils {
 
 	public static void ThrowUnsupportedOperationException() throws UnsupportedOperationException {
 		throw new UnsupportedOperationException("operation is not supported");
-	}
-
-	public static <T extends Exception> void throwException(Class<T> exceptionClass, String message) throws T {
-		try {
-			// Use reflection to create a new instance of the exception with the message
-			T exceptionInstance = exceptionClass.getConstructor(String.class).newInstance(message);
-			throw exceptionInstance;
-		} catch (ReflectiveOperationException e) {
-			// Handle case where the exception type doesn't have the expected constructor
-			throw new IllegalArgumentException("Invalid exception type provided", e);
-		}
-	}
+	}    /**
+     * Utility method for simulating exceptions and testing error handling.
+     * This method is for demonstration purposes only and allows dynamic creation
+     * and throwing of specified exception types.
+     *
+     * @param <T> The type of exception to throw
+     * @param exceptionClass The class of the exception to be thrown
+     * @param message The error message for the exception
+     * @throws T The specified exception type
+     */
+    public static <T extends Exception> void throwException(Class<T> exceptionClass, String message) throws T {
+        try {
+            // Use reflection to create a new instance of the exception with the message
+            T exceptionInstance = exceptionClass.getConstructor(String.class).newInstance(message);
+            throw exceptionInstance;
+        } catch (ReflectiveOperationException e) {
+            // Handle case where the exception type doesn't have the expected constructor
+            throw new IllegalArgumentException("Invalid exception type provided", e);
+        }
+    }
 }
